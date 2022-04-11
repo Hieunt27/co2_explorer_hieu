9ad3058c978db6d84417d8215ef402502a0dd1ef
Hieunt27 <59383378+Hieunt27@users.noreply.github.com>
2022-04-11 19:40:29 +0200

    Update CO2_explorer.py

--- CO2_explorer.py
+++ CO2_explorer.py
@@ -274,8 +274,9 @@ def update_scatter(xvar, d = indicators, df = df_2018):
     return fig
 
 if __name__ == '__main__':
-    app.title = 'CO2 explorer'
     app.run_server(debug=True)
+    
+app.title = 'CO2 explorer'
 
 #app.run_server()
 
